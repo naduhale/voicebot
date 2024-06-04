6e418d0f3419598aef62467d02370bd8f8602aa6
naduhale <39323838+naduhale@users.noreply.github.com>
2024-06-04 15:57:16 +0900

    Add files via upload

--- voicebot.py
+++ voicebot.py
@@ -105,7 +105,7 @@ def main():
 
         # Open AI API 키 입력받기
         openai.api_key = st.text_input(label="OPENAI API 키", placeholder="Enter Your API Key", 
-                                       value="sk-proj-Lfc3CQnj7zNxPZE2dS7GT3BlbkFJp05QUnwlGvcktey2PC3D", type="password")
+                                       value="", type="password")
 
         st.markdown("---")
 
